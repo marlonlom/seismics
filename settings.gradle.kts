b7a1e1c72e5b90e47fcb2918c494fb469b58081f
Marlon López <malm.gm@gmail.com>
2025-03-09 22:43:52 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2025 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+pluginManagement {
+  repositories {
+    google {
+      content {
+        includeGroupByRegex("com\\.android.*")
+        includeGroupByRegex("com\\.google.*")
+        includeGroupByRegex("androidx.*")
+      }
+    }
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+rootProject.name = "Seismics"
+include(":app:mobile")
