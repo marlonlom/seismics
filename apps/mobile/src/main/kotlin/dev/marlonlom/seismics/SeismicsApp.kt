511b8a1a862e55575596ef772aac308130e48661
Marlon López <malm.gm@gmail.com>
2025-03-29 22:05:26 -0500

    feat(apps:mobile): Setup koin library

--- apps/mobile/src/main/kotlin/dev/marlonlom/seismics/SeismicsApp.kt
+++ apps/mobile/src/main/kotlin/dev/marlonlom/seismics/SeismicsApp.kt
@@ -5,10 +5,29 @@
 package dev.marlonlom.seismics
 
 import android.app.Application
+import dev.marlonlom.seismics.di.appKoinModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 /**
  * Seismics mobile application main class.
  *
  * @author marlonlom
  */
-class SeismicsApp : Application()
+class SeismicsApp : Application() {
+
+  override fun onCreate() {
+    super.onCreate()
+    this.setupKoin()
+  }
+
+  private fun setupKoin() {
+    startKoin {
+      androidContext(this@SeismicsApp)
+      androidLogger(Level.DEBUG)
+      modules(appKoinModule)
+    }
+  }
+}
---
+++ apps/mobile/src/main/kotlin/dev/marlonlom/seismics/di/AppKoinModule.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2025 Marlonlom
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package dev.marlonlom.seismics.di
+
+import org.koin.dsl.module
+
+/**
+ * Main mobile application Koin module.
+ *
+ * @author marlonlom
+ */
+val appKoinModule = module {
+}
