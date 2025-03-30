71c4a833c0f3a60610ae24f6ebfcf115a9dece00
Marlon López <malm.gm@gmail.com>
2025-03-30 13:56:48 -0500

    feat(apps:mobile): integrate desygnsystem module into mobile app

--- apps/mobile/build.gradle.kts
+++ apps/mobile/build.gradle.kts
@@ -6,10 +6,13 @@
 plugins {
   id("seismics.android.application")
   id("seismics.android.app.compose")
+  id("seismics.android.koin")
   id("seismics.spotless")
 }
 
 dependencies {
+  implementation(project(":features:mobile:designsystem"))
+
   implementation(libs.androidx.lifecycle.runtime.ktx)
   implementation(libs.maps.compose)
   implementation(libs.play.services.maps)
