4737bc68d18f6dd7dcbde8ad53cfc8535e0ae387
Marlon López <malm.gm@gmail.com>
2025-06-23 11:21:56 -0500

    delete(core:network): delete ktor client logging configuration

--- features/core/network/src/main/kotlin/dev/marlonlom/seismics/core/network/config/KtorHttpClient.kt
+++ features/core/network/src/main/kotlin/dev/marlonlom/seismics/core/network/config/KtorHttpClient.kt
@@ -9,9 +9,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.observer.ResponseObserver
 import io.ktor.client.request.header
 import io.ktor.http.ContentType
@@ -55,15 +52,6 @@ internal val ktorHttpClient = HttpClient(Android) {
   engine {
     connectTimeout = TIME_OUT
     socketTimeout = TIME_OUT
-  }
-
-  install(Logging) {
-    logger = object : Logger {
-      override fun log(message: String) {
-        Log.d("ktorHttpClient", "Ktor Logging => $message")
-      }
-    }
-    level = LogLevel.ALL
   }
 
   install(ResponseObserver) {
